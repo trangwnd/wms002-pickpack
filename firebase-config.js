fb35efa839bcd520e00c35ade999b573fde06d29
trangwnd <trangwnd@gmail.com>
2025-07-29 08:54:13 +0700

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,9 @@
 export const firebaseConfig = {
-  databaseURL: "https://wms002-pickpack-aefd7-default-rtdb.asia-southeast1.firebasedatabase.app"
+  apiKey: "AIzaSyD5Y-ZtktBoq0qJ1EygYtTD-fbZqM54e3o",
+  authDomain: "wms002-pickpack-aefd7.firebaseapp.com",
+  databaseURL: "https://wms002-pickpack-aefd7-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "wms002-pickpack-aefd7",
+  storageBucket: "wms002-pickpack-aefd7.appspot.com",
+  messagingSenderId: "717973235210",
+  appId: "1:717973235210:web:c8c1f9ed9ed1fb6fd17a17"
 };
